f7d572cb2e96da00badfd7a53310bbfe09df474f
Miguel Piedrafita <soy@miguelpiedrafita.com>
2018-08-17 15:07:01 +0200

    Optimize CSS

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,11 @@
 const mix = require('laravel-mix');
 require('laravel-mix-tailwind');
+require('laravel-mix-purgecss');
 
 mix.setPublicPath('public')
    .js('resources/js/index.js', 'public/js')
    .less('resources/less/index.less', 'public/css')
-   .tailwind();
+   .tailwind()
+   .purgeCss({
+       folders: ['public']
+   });
